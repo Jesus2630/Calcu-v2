ee53eb6479815557189c69c76f0be54321c60f93
Jesus2630 <jesus.fg2630@gmail.com>
2022-06-20 16:53:19 -0300

    Subo los archivos principales

---
+++ main.js
@@ -0,0 +1,52 @@
+window.addEventListener("load", () => {
+    const display = document.querySelector(".c_display");
+    const btns = document.getElementsByClassName("c-boton");
+
+    const tecladoButtonArray = Array.from(btns);
+
+
+    tecladoButtonArray.forEach((button) =>{
+        button.addEventListener("click", ()=>{
+            calculadora(button, display);
+        })
+    })
+})
+
+function calculadora(button, display){
+    switch (button.innerHTML){
+        case "C":
+            borrar(display);
+            break;
+
+        case "=":
+            caclcular(display);
+            break;
+
+        default:
+            actualizar(display, button);
+            break;
+    }
+}
+
+function caclcular(display){
+    display.innerHTML = eval(display.innerHTML)
+}
+
+function actualizar(display,button){
+    if(display.innerHTML == 0){
+        display.innerHTML = "";
+    }
+    display.innerHTML += button.innerHTML;
+}
+
+function borrar (display){
+    display.innerHTML = 0;
+}
+
+/* Pruebas */
+const toggle = document.getElementById("toggle");
+const fondo = document.getElementsByClassName("calculadora")
+
+toggle.onclick = function estilo (){
+    
+}
